70c666ddb042c236ad3edcb1d2f8a862a6be29ce
WCook6nrw <WCook6nrw@users.noreply.github.com>
2021-05-05 09:43:06 +0100

    Added frontend
    Run Main in order to run the game.
    Added the front end using swing.
    Updated GameState so that it now holds which player's turn it is, and automatically updates.

---
+++ Connect_4_V0.02/src/connect/main/Main.java
@@ -0,0 +1,10 @@
+package connect.main;
+
+import connect.frontend.FrontEnd;
+
+public class Main {
+	public static void main(String args[]) {
+		FrontEnd front = new FrontEnd();
+		front.run();
+	}
+}
---
+++ Connect_4_V0.02/src/module-info.java
@@ -0,0 +1,3 @@
+module LocalProject {
+	requires java.desktop;
+}
