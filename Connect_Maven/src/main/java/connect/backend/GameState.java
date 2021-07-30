0815755f50fd9b33334ebb8f0edfbe7e884ae637
Mohamed-abc <76450218+Mohamed-abc@users.noreply.github.com>
2021-07-30 23:41:13 +0100

    Update GameState.java

--- Connect_Maven/src/main/java/connect/backend/GameState.java
+++ Connect_Maven/src/main/java/connect/backend/GameState.java
@@ -2,40 +2,49 @@ package connect.backend;
 
 public class GameState {
 	//this list will keep track of how many tokens there are in a specific column
-	static int [] columnState = {5, 5, 5, 5, 5, 5, 5};
-	
+	int [] columnState = {5, 5, 5, 5, 5, 5, 5};
+
 	// This matrix represents the board, the number 0 represents an unclaimed spot,
 	//1 will be yellow and 2 will be red tokens respectively
 	//The columns will be named:  0, 1, 2, 3, 4, 5, 6
-	static int [] [] board = {   {0, 0, 0, 0, 0, 0, 0},
-            		       		 {0, 0, 0, 0, 0, 0, 0},
-            		       		 {0, 0, 0, 0, 0, 0, 0},
-            		       		 {0, 0, 0, 0, 0, 0, 0},
-            		       		 {0, 0, 0, 0, 0, 0, 0},
-            		       		 {0, 0, 0, 0, 0, 0, 0}
-        				};
-		
-	public void addToken(int column, int colour) { 
-		board [columnState[column]] [column] = colour;
-		columnState[column] = columnState[column] - 1;
-		
-		for (int i = 0; i < board.length; i++) {
-	         for (int j = 0; j < board[i].length; j++) { 
-	            System.out.print(board[i][j] + " ");
-	         };
-	       System.out.println();
-	
-		}
-
-		System.out.println();
-		
-		
-		System.out.println("current winner: " + winCheck() + " (0 = none yet, 1 = yellow, 2 = red)");
-		
-		
+	int [] [] board = {   {0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0},
+			{0, 0, 0, 0, 0, 0, 0}
 	};
-	
-	
+
+	int playerTurn = 1;
+
+	public void addToken(int column) { 
+		if (columnState[column]>=0) {
+			board [columnState[column]] [column] = playerTurn;
+			columnState[column] = columnState[column] - 1;
+			
+			if (playerTurn==1) {
+				playerTurn = 2;
+			} else {
+				playerTurn = 1;
+			}
+
+			for (int i = 0; i < board.length; i++) {
+				for (int j = 0; j < board[i].length; j++) { 
+					System.out.print(board[i][j] + " ");
+				};
+				System.out.println();
+
+			}
+
+			System.out.println();
+
+
+			System.out.println("current winner: " + winCheck() + " (0 = none yet, 1 = yellow, 2 = red)");
+
+		}
+	};
+
+
 	public static int winCheck(){
 	int yCounter = 0;
 	int xCounter = 0;
@@ -140,10 +149,7 @@ public class GameState {
 
 	 return 0;
 	
-}
-		
-	
-
+	}
 	public int[] [] getBoard() {
 		return board;
 	}
