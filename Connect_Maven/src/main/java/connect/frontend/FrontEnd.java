fcb3744c5843013aa1e8b7ec0f0f3fbf46a2f13d
Mohamed-abc <76450218+Mohamed-abc@users.noreply.github.com>
2021-07-31 10:57:26 +0100

    Update FrontEnd.java

--- Connect_Maven/src/main/java/connect/frontend/FrontEnd.java
+++ Connect_Maven/src/main/java/connect/frontend/FrontEnd.java
@@ -55,7 +55,6 @@ public class FrontEnd {
 
 	// this method is run when there is a winner
 	public void end() {
-		Thread.sleep(4000);
 		// removes all panels, then adds panel with label displaying winner
 		frame.getContentPane().removeAll();
 
